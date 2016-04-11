7055b638bee9177c17d4f04d7ca612f5ba635815
Ned Henry <ethenry@ucsc.edu>
2016-04-11 15:34:45 -0700

    made top-level exhibit pages not break the site

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -8,10 +8,10 @@ echo head(array(
 	<h1><?php echo link_to_exhibit(); ?></h1>
 
 	<?php $childPages =  $exhibit_page->getChildPages();?>
-	<?php $exhibit_page =  ($exhibit_page->parent_id) ? $exhibit_page : $childPages[0];?>
+	<?php $exhibit_page =  ($exhibit_page->parent_id) || !isset($childPages[0]) ? $exhibit_page : $childPages[0];?>
 
 	<div id="nav-container">
-    		<?php echo exhibit_builder_page_nav($exhibit_page);?>
+    	  <?php echo exhibit_builder_page_nav($exhibit_page);?>
 	</div>
 
 	<h2><?php echo $exhibit_page->title; ?></h2>
